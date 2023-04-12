04255e16236d8834c2e5c360a485792eb0cff8f6
r-sasaki <r-sasaki@tribeat.com>
2023-04-12 14:50:16 +0900

    Add login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -64,6 +65,55 @@ class UserController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
             throw $e;
+        }
+    }
+
+    /**
+     * ユーザの認証
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required|min:6|max:255',
+        ],[
+            'email.required'    => 'メールアドレスは必須です',
+            'email.email'       => '有効なメールアドレスを入力してください',
+            'password.required' => 'パスワードは必須です',
+            'password.min'      => 'パスワードは6文字以上で入力してください',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            // 認証成功
+            $user = auth()->user();
+
+            $token = $this->userService->generateToken(
+                $credentials["email"],
+                $credentials["password"]
+            );
+
+            $data = [
+                "id" => $user->id,
+                "username" => $user->username,
+                "token" => $token,
+            ];
+
+            return response()->json([
+                "message" => "",
+                "code" => 200,
+                "data" => $data,
+            ]);
+        } else {
+            // 認証失敗
+            return response()->json([
+                "message" => "ユーザー認証に失敗しました",
+                "code" => 401,
+                "data" => null,
+            ]);
         }
     }
 }
