cb5d89f02ff1053d61a8b32c5056323d376066e9
e-ichinose <e-ichinose@tribeat.com>
2023-04-13 14:53:43 +0900

    リファクタリング

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    "required" => ":attributeは必須です。",
+    "email" => "有効なメールアドレスを入力してください。",
+    "min" => [
+        "string" => ":attributeは:min文字以上で入力してください。",
+    ],
+    "max" => [
+        "string" => ":attributeは:max文字以下で入力してください。",
+    ],
+
+    "attributes" => [
+        "username" => "ユーザ名",
+        "email" => "メールアドレス",
+        "password" => "パスワード",
+    ],
+];
